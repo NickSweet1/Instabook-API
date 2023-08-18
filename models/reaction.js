f9bd8870bcd9b85f6bda76b26c0a53d45659a8fc
Nick <nicksweetj@gmail.com>
2023-08-17 23:17:27 -0400

    completed the modules folder. More imporvement may be needed in the furture

---
+++ models/reaction.js
@@ -0,0 +1,23 @@
+const { Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId, // Use Mongoose's ObjectId data type
+    default: new Schema.Types.ObjectId(), // Set default value to a new ObjectId
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Reaction;
---
+++ models/user.js
@@ -0,0 +1,54 @@
+const { Schema, Types } = require("mongoose");
+
+var validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, "Invalid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Invalid email address",
+      ],
+    },
+    thought: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
