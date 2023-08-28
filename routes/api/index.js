5c6fc53152ae5b6f56b9daa0516ec759d78bb5c3
Nick <nicksweetj@gmail.com>
2023-08-28 14:43:13 -0400

    removed unecessary code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,10 @@
 const router = require("express").Router();
 const thoughtRoutes = require("./thoughtRoutes");
 const userRoutes = require("./userRoutes");
-// const friendsRoutes = require("./friendsRoutes");
-// const reactionsRoutes = require("./reactionsRoutes");
+
 
 router.use("/thoughts", thoughtRoutes);
 router.use("/users", userRoutes);
-// router.use("/users/:userId/friends/:friendId", friendsRoutes);
-// router.use("/thoughts/:thoughtId/reactions", reactionsRoutes);
+
 
 module.exports = router;
